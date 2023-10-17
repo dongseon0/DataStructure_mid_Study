703a99779b949e7658ce31d37348a78c923a5658
Dongseon Yang <78548833+dongseon0@users.noreply.github.com>
2023-10-18 00:36:51 +0900

    Update Lec03_Array_pointer.c

--- Lec03_Array_pointer.c
+++ Lec03_Array_pointer.c
@@ -118,8 +118,73 @@ void sparse_matrix_add1(int A[ROWS][COLS], int B[ROWS][COLS], int c[ROWS][COLS])
     }
 }*/
 /******** Sparse Matrix (only non-zero elements)*/
+#include <stdio.h>
+#define ROWS 3
+#define COLS 3
+#define MAX_TERMS 10
+typedef struct{
+    int row;
+    int col;
+    int value;
+}element;
+typedef struct SparseMatrix{
+    element data[MAX_TERMS];
+    int rows;
+    int cols;
+    int terms; //number of elements(non-zero values)
+}SparseMatrix;
+//Addition
 
+//이거 이해가 안됨....
 
+SparseMatrix sparse_matrix_add2(SparseMatrix a, SparseMatrix b){
+    SparseMatrix c;
+    int ca=0, cb=0, cc=0; //index indicating (non-zero)terms in each array
+    //array a, array b의 크기가 같은지 확인
+    if(a.rows!=b.rows || a.cols!= b.cols){
+        fprintf(stderr, "Size Error");
+        exit(1);
+    }
+    c.rows = a.rows;
+    c.cols = a.cols;
+    c.terms = 0;
+
+    while(ca<a.terms && cb<b.terms){
+        int inda = a.data[ca].row * a.cols + a.data[ca].col;
+        int indb = b.data[cb].row * b.cols = b.data[cb].col;
+    //if array 'a' entry is in front
+    if(inda < indb){
+        c.data[cc++] = a.data[ca++];
+
+    }else if (inda == indb){
+        if((a.data[ca].value + b.data[cb].value ! = 0)){
+            c.data[cc].row = a.data[ca].row;
+            c.data[cc].col = a.data[ca].col; 
+            c.data[cc++].value = a.data[ca++].value + b.data[cb++].value;
+        }else
+            ca++;  cb++;
+    //if array 'b' entry is in front
+    }else{ 
+        c.data[cc++] = b.data[cb++];
+    }
+}
+    //copy and paste
+        for(; ca< a.terms;){
+            c.data[cc++] = a.data[ca++];
+        }
+        for(; cb< b.terms;){
+            c.data[cc++] = b.data[cb++];
+        }
+        c.terms = cc;
+        return c;
+    }
+
+main(){
+    SparseMatrix m1 = {{{1,1,5},{2,2,9}}, 3, 3, 2};
+    SparseMatrix m2 = {{{0,0,5},{2,2,9}}, 3, 3, 2};
+    SparseMatrix m3;
+    m3 = sparse_matrix_add2(m1, m2);
+}
 
 
 
